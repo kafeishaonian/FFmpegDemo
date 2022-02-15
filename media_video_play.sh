fd902a613f0bd745db82e2b4b6aa725c2be213cc
kafeishaonian <648600445@qq.com>
2022-02-15 14:14:17 +0800

    添加打包脚本

--- media_video_play.sh
+++ media_video_play.sh
@@ -1,9 +1,65 @@
 #!/bin/bash
 
-if [ -n "$(git status --porcelain)" ]; then
-  echo "还有未提交的，请现处理";
+MAIN_BEANCH_PATH="master"
+BRANCH_PROJECT="$(git rev-parse --abbrev-ref HEAD)"
+
+## 执行打包脚本
+set -e
+hitPack(){
+  cd ../..
+  ./gradlew :modules:momo-ui:clean :modules:momo-ui:uploadArchives
+}
+
+
+## 提示push代码远端
+isCommit(){
+  if [ -n "$(git status --porcelain)" ]; then
+    echo "还有未提交的，请现处理";
+    exit 1
+  else
+      echo "已全部提交";
+      checkoutMain
+  fi
+}
+
+## 切换到主分枝并merge开发分支到主分枝并打包
+checkoutMain(){
+  git pull --rebase
+  echo "当前分支 $BRANCH_PROJECT 已是最新代码"
+
+  git checkout $MAIN_BEANCH_PATH
+  git pull --rebase
+
+  if [ -n "$(git merge BRANCH_PROJECT)" ]; then
+    echo "存在冲突，请解决"
+    exit 1
+  else
+    hitPack
+  fi
+}
+
+
+## 解决冲突之后，重新运行当前脚本
+mainBranchCommit(){
+  if [ -n "$(git merge BRANCH_PROJECT)" ]; then
+    git add .
+    git commit
+    git push origin $MAIN_BEANCH_PATH
+    hitPack
+  fi
+}
+
+
+if [ "$BRANCH_PROJECT" = "$MAIN_BEANCH_PATH" ]; then
+  if [ -n "$(git status --porcelain)" ]; then
+    mainBranchCommit
+  else
+    hitPack
+  fi
+  exit 1
 else
-    echo "已全部提交";
+  echo "当前分支： $BRANCH_PROJECT";
+  isCommit
 fi
 
 
@@ -11,37 +67,7 @@ fi
 
 
 
-#BRANCH_PROJECT=""
-#
-#git checkout master
-#
-#git pull --rebase
-#
-#set -e
-#info(){
-#    echo "./media_video_play.sh [-b] branchName [v] version"
-#    echo "b 指定从那个分支merge到主分枝，"
-#    echo "v 版本号，不需要输入，默认版本号规则为 xxxSDK.20220214181520"
-#}
-#
-## 获取输入参数
-#while getopts "b:v:" opt; do
-#    echo " ${opt}    ${OPTARG}  "
-#  case ${opt} in
-#    v)
-#      NEWVERSION=${OPTARG}
-#      ;;
-#    b)
-#      BRANCH_PROJECT=${OPTARG}
-#      ;;
-#    ?)
-#      echo "error: 无效参数"
-#      info
-#      exit 1
-#  esac
-#done
-#
-#if [ $BRANCH_PROJECT -ne "" ]; then
-#  git merge $BRANCH_PROJECT
+
+
 
 
