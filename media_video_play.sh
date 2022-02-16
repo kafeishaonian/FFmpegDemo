2091f6c3011064ee5669128b9e5166e27f35ce59
kafeishaonian <648600445@qq.com>
2022-02-16 18:38:46 +0800

    打包脚本提交

--- media_video_play.sh
+++ media_video_play.sh
@@ -9,6 +9,7 @@ MAIN_BRANCH_PATH=""
 
 MODULES_FIRST_NAME=""
 MODULES_LAST_NAME=""
+NEW_VERSION=""
 
 
 set -e
@@ -26,7 +27,7 @@ fi
 
 
 # 获取输入参数
-while getopts "m:d:b:" opt; do
+while getopts "m:d:b:v:" opt; do
   echo " ${opt}    ${OPTARG}"
   case ${opt} in
   m)
@@ -37,6 +38,9 @@ while getopts "m:d:b:" opt; do
     ;;
   b)
     MAIN_BRANCH_PATH=${OPTARG}
+    ;;
+  v)
+    NEW_VERSION=${OPTARG}
     ;;
   ?)
     echo "error: 无效参数"
@@ -92,12 +96,15 @@ mergeConflictSolve(){
   git ls-files -u | cut -f 2 | sort -u
 
   read -p "解决冲突后按 Y，现在退出，N？ Y｜N" merge_params
-
-  git add .
-  git commit
-  git pull --rebase
-  if [ -n "$(git status --porcelain)" ]; then
-    mergeConflictSolve
+  if [[ $add_params == "Y" || $add_params == "y" ]]; then
+    git add .
+    git commit
+    git pull --rebase
+    if [ -n "$(git status --porcelain)" ]; then
+      mergeConflictSolve
+    fi
+  else
+    exit 1
   fi
 }
 
@@ -124,12 +131,12 @@ addCodePushOrigin(){
 # 对未提交代码进行处理
 statusCodeOperation(){
   read -p "当前有未提交代码，1, 继续提交， 2，暂存，  3， 舍弃  ？1|2｜3 : " operation_params
-  if [ ${operation_params} == "1" ]; then
+  if [ "${operation_params}" == "1" ]; then
     addCodePushOrigin
-  elif [ ${operation_params} == "2" ]; then
+  elif [ "${operation_params}" == "2" ]; then
     git stash
   else
-
+    git checkout .
   fi
 }
 
@@ -137,167 +144,69 @@ statusCodeOperation(){
 isStatus(){
   if [ -n "$(git status --porcelain)" ]; then
     statusCodeOperation
-  else
-      echo "已全部提交";
-      checkoutMain
+  fi
+}
+
+#打包
+hitPack(){
+  cd ../..
+  ./gradlew "${MODULES_NAME}":clean "${MODULES_NAME}":uploadArchives
+}
+
+#合并分支
+mergeDevelopmentBranch(){
+  git merge "${DEV_BRANCH_NAME}"
+  if [ -n "$(git status --porcelain)" ]; then
+    mergeConflictSolve
   fi
 }
 
 
-
-
+gitTagAndLog(){
+  if [ "${MAIN_BRANCH_PATH}" == "${BRANCH_PROJECT}" ]; then
+    #记录编译时间，版本号， commitid， 信息至文件，方便查询
+    BUILD_INFO="对 ${MODULES_NAME} 发布版本，版本号详见tag"
+    echo 'back git commit'
+    set -x
+    echo -e '\n' >> "${MODULES_FIRST_NAME}"/"${MODULES_LAST_NAME}"/"${MODULES_LAST_NAME}_log.txt"
+    echo "$BUILD_INFO" >> "${MODULES_FIRST_NAME}"/"${MODULES_LAST_NAME}"/"${MODULES_LAST_NAME}_log.txt"
+    echo "$current_date_time" >> "${MODULES_FIRST_NAME}"/"${MODULES_LAST_NAME}"/"${MODULES_LAST_NAME}_log.txt"
+    git rev-parse --abbrev-ref HEAD >> "${MODULES_FIRST_NAME}"/"${MODULES_LAST_NAME}"/"${MODULES_LAST_NAME}_log.txt"
+    git rev-parse HEAD >> "${MODULES_FIRST_NAME}"/"${MODULES_LAST_NAME}"/"${MODULES_LAST_NAME}_log.txt"
+    git log --pretty=format:'%D %ci%n' -1 >> "${MODULES_FIRST_NAME}"/"${MODULES_LAST_NAME}"/"${MODULES_LAST_NAME}_log.txt"
+  fi
+}
 
 
 
 BRANCH_PROJECT="$(git rev-parse --abbrev-ref HEAD)"
 
-
-
-
 mainBranch(){
+  # 主分支与当前分支一致
   if [ "${MAIN_BRANCH_PATH}" == "${BRANCH_PROJECT}" ]; then
     isStatus
-
-
-
-
-  else
-
-
-
-
+    if [ "${DEV_BRANCH_NAME}" != "" ]; then
+      git checkout "${DEV_BRANCH_NAME}"
+      git pull --rebase
+      git checkout "${MAIN_BRANCH_PATH}"
+      mergeDevelopmentBranch
+    fi
+    hitPack
+  elif [ "${DEV_BRANCH_NAME}" == "${BRANCH_PROJECT}" ]; then
+  #当前分支与主分枝相同
+    isStatus
+    if [ "${MAIN_BRANCH_PATH}" != "" ]; then
+      git checkout "${MAIN_BRANCH_PATH}"
+      mergeDevelopmentBranch
+    fi
+    hitPack
+  elif [[ "${MAIN_BRANCH_PATH}" == "" || "${DEV_BRANCH_NAME}" == "" ]]; then
+  # 主分支与合并分支都为空时, 直接提交代码并打包
+    isStatus
+    hitPack
   fi
-  
-  
-  
-  
+  gitTagAndLog
 }
-
-
-
-
-
-
-
-# 主分枝判断   在   直接处理     不在  查看是否未提交    然后切换到主分枝    然后更新数据   pull
-
-# 输入合并分支 
-
-
-
-
-# 首先判断是否输入主分枝， 输入主分枝  判断当前分支是否在主分枝， 在主分枝   判断是否还有未提交代码   如果有就提交，或者舍弃， 或者暂存，  如果没有就询问是否 直接打包
-# 在输入主分枝的情况下
-
-#
-
-# 首先判断是否输入开发分支名称， 如果输入   则判断当前是否当前分支， 在开发分支之后， 判断是否还有未提交代码   如果有就提交，或者舍弃， 或者暂存，  如果没有就询问是否 直接打包
-
-#如果当前分支不在开发分支， 现判断是否有未提交代码， 如果有   要么提交，  要么舍弃  要不暂存    如果没有    则直接切换分支，   并询问是否打包
-
-
-# 是否输入主分枝，
-
-
-
-#if [  ]; then
-#
-#fi
-
-
-
-read -p "是否操作当前分支 ${BRANCH_PROJECT} ？Y|N : " add_params
-if [[ $add_params == "Y" || $add_params == "y" ]]; then
-
-else
-  #请
-
-
-  exit
-fi
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-### 执行打包脚本
-#set -e
-#hitPack(){
-#  cd ../..
-#  ./gradlew :modules:momo-ui:clean :modules:momo-ui:uploadArchives
-#}
-#
-#
-### 提示push代码远端
-#isCommit(){
-#  if [ -n "$(git status --porcelain)" ]; then
-#    echo "还有未提交的，请现处理";
-#    exit 1
-#  else
-#      echo "已全部提交";
-#      checkoutMain
-#  fi
-#}
-#
-### 切换到主分枝并merge开发分支到主分枝并打包
-#checkoutMain(){
-#  git pull --rebase
-#  echo "当前分支 $BRANCH_PROJECT 已是最新代码"
-#
-#  git checkout $MAIN_BEANCH_PATH
-#  git pull --rebase
-#
-#  if [ -n "$(git merge BRANCH_PROJECT)" ]; then
-#    echo "存在冲突，请解决"
-#    exit 1
-#  else
-#    hitPack
-#  fi
-#}
-#
-#
-### 解决冲突之后，重新运行当前脚本
-#mainBranchCommit(){
-#  if [ -n "$(git merge BRANCH_PROJECT)" ]; then
-#    git add .
-#    git commit
-#    git push origin $MAIN_BEANCH_PATH
-#    hitPack
-#  fi
-#}
-#
-#
-#if [ "$BRANCH_PROJECT" = "$MAIN_BEANCH_PATH" ]; then
-#  if [ -n "$(git status --porcelain)" ]; then
-#    mainBranchCommit
-#  else
-#    hitPack
-#  fi
-#  exit 1
-#else
-#  echo "当前分支： $BRANCH_PROJECT";
-#  isCommit
-#fi
 
 
 
